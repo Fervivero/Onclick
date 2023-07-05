14f222d3a22cf77d00ec265346e536a9c96fad70
Fernanda Vives <mfernandavives@gmail.com>
2023-07-04 21:42:39 -0400

    Se sube versión 1 de la tarea.

---
+++ script.js
@@ -0,0 +1,12 @@
+function saludo1(elemento) {
+    elemento.innerText = "Logout";
+}
+
+function saludo2(elemento) {
+    console.log(elemento);
+    alert("Ninja was liked");
+}
+
+function saludo3(elemento) {
+    elemento.remove();
+}
